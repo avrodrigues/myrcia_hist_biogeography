262505c3a9bae47875089c28c1223a9a9fd84f49
Arthur Rodrigues <rodrigues.arthur.v@gmail.com>
2023-12-14 14:10:42 +0200

    make Fig. S4 - ancestral recontruction with pie chart

--- script/fig/fig_03_anc_reconstruction_phy_consensus.R
+++ script/fig/fig_03_anc_reconstruction_phy_consensus.R
@@ -162,9 +162,12 @@ range_cols_2  <-
     "E" = "#BB4455",
     
     "AB" = "#5A7C9F",
+    "AC" = "#3e9d57",
     
     "BC" =  "#90c5a4",
     "BCD" = "#545c57",
+    "BD" = "#286c2e",
+    
     "BCE" =  "#88975D",
     "BE" = "#674ab0",
     
@@ -183,11 +186,6 @@ tipranges <- BioGeoBEARS::getranges_from_LagrangePHYLIP(lgdata_fn = geog.path)
 trtable <- BioGeoBEARS::prt(myrcia_tree, printflag=FALSE)
 areas <- BioGeoBEARS::getareas_from_tipranges_object(tipranges)
 prob_state <- l_biogeo_mod$resDECj$ML_marginal_prob_each_state_at_branch_top_AT_node
-
-
-
-
-
 states_list_0based <- cladoRcpp::rcpp_areas_list_to_states_list(areas = areas, 
                                                                 maxareas = 3, 
                                                                 include_null_range = TRUE)
@@ -207,45 +205,132 @@ row.names(range_probabilities) <- trtable$node
 names(range_probabilities) <- ranges_list
 
 prob_nodes <- range_probabilities |> 
-  mutate(node = row_number()) |> 
+  mutate(node = row_number(), .before = 1) |> 
   filter(!node %in% 1:96)
 
-vec_length <- ncol(prob_nodes)
-prob_vec <- prob_nodes[1,-vec_length] |> 
-  sort(decreasing = T) |> 
-  suppressWarnings()
-    
-n_most <- 3
-
-most_prob <- prob_vec[1:n_most]
-sum_other_prob <- sum(prob_vec[-c(1:n_most)])
-
-chart_df <- data.frame(
-  ranges = c(names(most_prob), "other"),
-  prob = c(as.numeric(most_prob), sum_other_prob),
-  node = prob_nodes[1, vec_length]
-) |> 
-  arrange(desc(prob))
-
-rm_other <- str_detect(chart_df$ranges, "other", negate = TRUE)
-
-chart_df$ranges <- factor(chart_df$ranges, levels = c(chart_df$ranges[rm_other], "other"))
-
-ggplot(chart_df, aes(x = "", y=prob, fill=ranges)) +
-  geom_bar(width = 1, stat = "identity", color="white") +
-  coord_polar("y", start=3.14, direction = -1) +
-  scale_fill_manual(values = range_cols_2) +
-  theme_void()
-
+pie_chart_df <- 
+apply(prob_nodes, 1, function(node){
+  prob_vec <-  node[-1] |> sort(decreasing = T)
+  
+  n_most <- 3
+  
+  most_prob <- prob_vec[1:n_most]
+  sum_other_prob <- sum(prob_vec[-c(1:n_most)])
+  
+  chart_df <- data.frame(
+    ranges = c(names(most_prob), "other"),
+    prob = c(as.numeric(most_prob), sum_other_prob),
+    node = node[1]
+  ) |> 
+    arrange(desc(prob))
+  
+  chart_df
+}) |> list_rbind() |> 
+  pivot_wider(
+    names_from = "ranges", 
+    id_cols = "node", 
+    values_from = prob,
+    values_fill = 0
+  )
+  
 
 
-nodepie(prob_nodes, 1:(vec_length-1), outline.color = "white", )
 
 
-colSums(range_probabilities)>0
-col_mtx <- col2rgb(range_cols_2[1:5])
-
-rowMeans(col_mtx)
-rowMeans(col_mtx/255)
+pies <- nodepie(pie_chart_df, 2:ncol(pie_chart_df), outline.color = "white",  outline.size = .15)
+pies <- lapply(pies, function(g) g+scale_fill_manual(values = range_cols_2))
 
 
+
+
+pie_tree <- ggtree(
+  tree_df, 
+  layout = "rect",
+  aes(x = x), 
+  color = "grey30",
+  size = 2,
+  show.legend = F
+) +
+  geom_tiplab(
+    color = "#202015", 
+    offset = 0.5, 
+    fontface = 3
+  ) +
+  geom_label(
+    aes(x = x, y = y, 
+        label = area, #rep(" ", 191), 
+        fill = area), 
+    color = "#ffffff", 
+    label.r = unit(0.01, "lines"), 
+    label.padding = unit(0.2, "lines"), 
+    size = 2.5, 
+    fontface = 2
+  ) +
+  scale_fill_manual(name = "Ranges", values = range_cols_2) +
+  scale_color_manual(name = "Ranges", values = range_cols_2) +
+  geom_inset(pies, width = .055, height = .055) +
+  scale_x_continuous(
+    limits = c(-30, 5), 
+    breaks = seq(-30, 5, 5), 
+    labels = c(paste0(seq(30, 0, -5), "Ma"), "") 
+  ) +
+  theme_tree2() +
+  theme(
+    legend.position = "bottom", 
+    legend.title = element_text(
+      size = 18
+    ),
+    legend.text = element_text(
+      size = 18
+    ),
+    axis.text = element_text(
+      size = 18
+    )
+  ) 
+
+
+ggsave(
+  "output/fig/S4_anc_rec_pie_chart.png",
+  pie_tree, 
+  width = 10, 
+  height = 15.8
+) 
+
+
+
+# creates a legend plot
+
+leg_df <- pie_chart_df[1,2:ncol(pie_chart_df)]
+
+
+leg_df <- data.frame(
+  area = factor(names(range_cols_2), levels = names(range_cols_2)), 
+  value = runif(13, 6,8)
+)
+
+leg_plot <- ggplot(leg_df, aes(x = 1, y = value, fill = area)) +
+  geom_bar(stat = "identity") +
+  coord_polar(theta = "y") +
+  scale_fill_manual(name = "Ranges", values = range_cols_2, 
+                    labels = c(names(range_cols_2)[-13], "Other")) +
+  theme(
+    legend.position = "bottom", 
+    legend.title = element_text(
+      size = 18
+    ),
+    legend.text = element_text(
+      size = 18
+    ),
+    axis.text = element_text(
+      size = 18
+    )
+  ) 
+
+leg_only <- ggpubr::as_ggplot(ggpubr::get_legend(leg_plot))
+
+ggsave(
+  "output/fig/S4_legend_pie_chart.png",
+  leg_only, 
+  width = 10, 
+  height = 2
+) 
